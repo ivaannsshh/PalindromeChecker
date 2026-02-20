3538461ffd5a180b9861753404c5def4fb5865f1
ivaannsshh <vanshbareilly782005@gmail.com>
2026-02-20 09:49:27 +0530

    UC3: Print a Hardcoded Palindrome Result

--- src/App.java
+++ src/App.java
@@ -1,22 +1,47 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+      public static boolean checkPalindrome(String input) {
+
+        // Convert String to char array
+        char[] charArray = input.toCharArray();
+
+        // Initialize two pointers
+        int start = 0;
+        int end = charArray.length - 1;
+
+        // Compare characters using two-pointer technique
+        while (start < end) {
+
+            if (charArray[start] != charArray[end]) {
+                return false;   // Not a palindrome
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;    // It is a palindrome
+    }
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("=== Palindrome Checker App (UC3) ===");
-        System.out.print("Enter a string to check: ");
-        String originalString = scanner.nextLine();
-        String reversedString = "";
-        for (int i = originalString.length() - 1; i >= 0; i--) {
-            reversedString = reversedString + originalString.charAt(i);
-        }
-        if (originalString.equals(reversedString)) {
-            System.out.println("Result: The given string is a PALINDROME.");
+
+        System.out.println("===== Palindrome Checker App - UC4 =====");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = checkPalindrome(input);
+
+        if (isPalindrome) {
+            System.out.println("Result: The given string is a Palindrome.");
         } else {
-            System.out.println("Result: The given string is NOT a palindrome.");
+            System.out.println("Result: The given string is NOT a Palindrome.");
         }
+
+    
+    
         scanner.close();
     }
 }
-
-
