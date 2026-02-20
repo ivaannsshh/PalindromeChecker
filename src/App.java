091f6cdfc3d582ab3c02e5ea061f8d6cf17e8fea
ivaannsshh <vanshbareilly782005@gmail.com>
2026-02-20 09:58:32 +0530

    UC3: Print a Hardcoded Palindrome Result

--- src/App.java
+++ src/App.java
@@ -1,47 +1,30 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class App {
-      public static boolean checkPalindrome(String input) {
-
-        // Convert String to char array
-        char[] charArray = input.toCharArray();
-
-        // Initialize two pointers
-        int start = 0;
-        int end = charArray.length - 1;
-
-        // Compare characters using two-pointer technique
-        while (start < end) {
-
-            if (charArray[start] != charArray[end]) {
+    public static boolean checkPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 return false;   // Not a palindrome
             }
-
-            start++;
-            end--;
         }
-
         return true;    // It is a palindrome
     }
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("===== Palindrome Checker App - UC4 =====");
+        System.out.println("===== Palindrome Checker App - UC5 (Stack Based) =====");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
         boolean isPalindrome = checkPalindrome(input);
-
         if (isPalindrome) {
             System.out.println("Result: The given string is a Palindrome.");
         } else {
             System.out.println("Result: The given string is NOT a Palindrome.");
         }
-
-    
-    
         scanner.close();
     }
 }
