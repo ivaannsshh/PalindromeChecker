0284a73bdc20c27a5c91c021fb2fc6a9405fbec8
ivaannsshh <vanshbareilly782005@gmail.com>
2026-02-20 09:27:09 +0530

    UC3: Print a Hardcoded Palindrome Result

--- src/App.java
+++ src/App.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word: ");
-        String word = scanner.nextLine();
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        System.out.println("=== Palindrome Checker App (UC3) ===");
+        System.out.print("Enter a string to check: ");
+        String originalString = scanner.nextLine();
+        String reversedString = "";
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + originalString.charAt(i);
         }
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        if (originalString.equals(reversedString)) {
+            System.out.println("Result: The given string is a PALINDROME.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("Result: The given string is NOT a palindrome.");
         }
         scanner.close();
     }
 }
+
 
